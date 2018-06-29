cc67da6c41ea54a16e50fe9f2ac5f6c78c5552e4
Charles <charlesharrison020201@Gmail.com>
2018-06-30 00:23:01 +0100

    Background Load Times
    Background.js is "browserify"ed to a file named compiled_background.js . Match data is sent from the background to the extension when loaded. This should (hopefully) reduce loading times and they should only apply if the extension is loaded the same time as chrome is opened.

--- app.js
+++ app.js
@@ -1,110 +1,50 @@
-const {
-    HLTV
-} = require(`hltv`)
-const countryCodes = require(`./countrycodes.json`)
+// const {
+//     HLTV
+// } = require(`hltv`)
 //const xyz = document.getElementById(`test`)
-// const body = document.getElementById(`body`)
-const liveMatches = []
-//This will be an array containing objects that will hold info about each team and match currently playing.
-let html = ``;
+const body = document.getElementById(`body2`)
+
+let template = `<div class="match-preview" id="TEAM1NAME"><div class="team-box"><div class="team"><img src="https://static.hltv.org/images/bigflags/300x200/TEAM1COUNTRY.png" class="team-L"><div class="team-L-gradient"><a href="hltv.org/team/TEAM1ID/TEAM1NAME"><img src="https://static.hltv.org/images/team/logo/TEAM1ID" class="logo"><div class="teamname"> TEAM1NAME </div></a><div class="results-l"><div class="result result-l"> MAP1_TEAM1 </div><div class="result result-w"> MAP2_TEAM1 </div><div class="result"> MAP3_TEAM1 </div></div></div></div><div class="middle"><div class="eventdetails"><div class="starttime">21:25</div><div class="eventname">EVENT_NAME</div></div><div class="maps"><div class="result"> MAP1_NAME </div><div class="result"> MAP2_NAME </div><div class="result"> MAP3_NAME </div></div></div><div class="team"><img src="https://static.hltv.org/images/bigflags/300x200/TEAM2COUNTRY.png" class="team-R"><div class="team-R-gradient"><a href="hltv.org/team/TEAM2ID/TEAM2NAME"><img src="https://static.hltv.org/images/team/logo/TEAM2ID" class="logo"><div class="teamname"> TEAM2NAME </div></a><div class="results-r"><div class="result result-w"> MAP1_TEAM2 </div><div class="result result-l"> MAP2_TEAM2 </div> <div class="result"> MAP3_TEAM2 </div></div></div></div></div></div>`
+const templates = []
 window.onload = function () {
     chrome.extension.getBackgroundPage().console.log(`Chrome extension has been loaded.`)
-    // body.innerHTML = `Chrome extension opened!`
-
-    HLTV.getMatches() //Get a list of the current matches on hltv.org/matches
-        .then((matches) => {
-            for (let i = 0; i < matches.length; i++) {
-                if (matches[i].live) {
-
-                    let liveMatch = matches[i]
-                    let matchObj = {
-                        matchid: liveMatch.id,
-                        team1: {
-                            name: liveMatch.team1.name,
-                            teamid: liveMatch.team1.id
-                        },
-                        team2: {
-                            name: liveMatch.team2.name,
-                            teamid: liveMatch.team2.id
-                        },
-                        eventName: liveMatch.event.name,
-                        format: liveMatch.format,
-                        maps: liveMatch.maps
-                    }
-                    liveMatches.push(matchObj) //Add all the live matches objects into one array.
-
+    let port = chrome.extension.connect({
+        name: "Sample Communication"
+    });
+    port.onMessage.addListener(function (data) {
+        chrome.extension.getBackgroundPage().console.log(`Data has been recieved.`)
+        body.innerHTML = ``
+        for (let i = 0; i < data.length; i++){
+            let match = data[i]
+            let htmlPreview = template 
+                //Team 1
+                .replace(/TEAM1NAME/g, match.team1.name)
+                .replace(/TEAM1COUNTRY/g, match.team1.countrycode)
+                .replace(/TEAM1ID/g, match.team1.teamid)
+                //Team 2
+                .replace(/TEAM2NAME/g, match.team2.name)
+                .replace(/TEAM2COUNTRY/g, match.team2.countrycode)
+                .replace(/TEAM2ID/g, match.team2.teamid)
+                //Map Scores
+                .replace(/MAP1_TEAM1/g, match.maps[0].split(":")[0])
+                .replace(/MAP2_TEAM1/g, match.maps[1].split(":")[0])
+                .replace(/MAP3_TEAM1/g, match.maps[2].split(":")[0])
+                .replace(/MAP1_TEAM2/g, match.maps[0].split(":")[1])
+                .replace(/MAP2_TEAM2/g, match.maps[1].split(":")[1])
+                .replace(/MAP3_TEAM2/g, match.maps[2].split(":")[1])
+                //Map Names
+                .replace(/MAP1_NAME/g, match.maps[0].name)
+                .replace(/MAP2_NAME/g, match.maps[1].name)
+                .replace(/MAP3_NAME/g, match.maps[2].name)
+            templates.push(htmlPreview)
+            if (data.length - 1 == i){
+                //Last run through - lets make the visuals - this compiles all in one go.
+                body.innerHTML = ``;
+                for (let v = 0; v < templates.length; v++){
+                    body.innerHTML += templates[v]
                 }
             }
-            //Now that we have a list of the live matches, we have to get more info about the teams.
-            for (let i = 0; i < liveMatches.length; i++) {
-                let matchObj = liveMatches[i]
-                /*
-                    prepare to enter callback hell
-
-                    For every live match, search up each team and get some data.
-                    -> HLTV Logo link
-                    -> Their country name, look up the country name in a json and find a country code.
-                */
-                HLTV.getMatch({
-                        id: matchObj.matchid
-                    })
-                    .then((match) => {
-                        chrome.extension.getBackgroundPage().console.log(`1`)
-                        matchObj.maps = match.maps;
-                        //This contains the score for each map.
-
-                        HLTV.getTeam({
-                                id: matchObj.team1.teamid
-                            })
-                            .then((teamA) => {
-                                matchObj.team1.logo = teamA.logo
-                                //We need to convert the location to a country code.
-                                for (let v = 0; v < countryCodes.length; v++) {
-                                    if (countryCodes[v].name.toLowerCase() == teamA.location.toLowerCase()) {
-                                        matchObj.team1.countrycode = countryCodes[v].code
-                                        break; //No need to keep looking for a match if we found one.
-                                    }
-                                }
-
-                                HLTV.getTeam({
-                                        id: matchObj.team2.teamid
-                                    })
-                                    .then((teamB) => {
-                                        matchObj.team2.logo = teamB.logo
-                                        //We need to convert the location to a country code.
-                                        for (let v = 0; v < countryCodes.length; v++) {
-                                            if (countryCodes[v].name.toLowerCase() == teamB.location.toLowerCase()) {
-                                                matchObj.team2.countrycode = countryCodes[v].code
-                                                break; //No need to keep looking for a match if we found one.
-                                            }
-                                        }
-
-                                        //Add all the existing information to the extension for now.
-                                        //xyz.innerHTML = JSON.stringify(liveMatches);
-                                        //body.innerHTML += `<div class="match-preview">${JSON.stringify(matchObj)}</div>`
-                                    })
-
-                            })
-                        // .catch((err) => { chrome.extension.getBackgroundPage().console.log(`${err}`) })
-                        // We could use .catch here, but when we don't use it we can just skip the team's country code being added,
-                        // bit of a bodge job but it'll do for now.
-                    })
-                    .catch((err) => {
-                        chrome.extension.getBackgroundPage().console.log(err)
-                    })
-
-
-            }
-        })
-
-
+            
+        }
+    });
 }
-/*
-    basic method
-    
-    Get list of matches,
-    Find which matches are live,
-    GetMatch for each
-        -Get Team for team 1 and team 2
-        
-*/
--- content.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(recievedMessage)
-
-function recievedMessage(message, sender, sendRes){
-    console.log(message.txt)
-}
